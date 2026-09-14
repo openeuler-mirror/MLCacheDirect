4e6667af74b9edac4b98d2f8d78d842c923cc10f
PhoenixVang <wangzhiyong41@huawei.com>
2026-09-14 19:49:56 +0800

    fix:问题修改

--- tools/datasystem_test/pipeline_async_pin_test.cpp
+++ tools/datasystem_test/pipeline_async_pin_test.cpp
@@ -197,7 +197,7 @@ public:
 
     cudaError_t Create()
     {
-        return cudaEventCreateWithFlags(&event_, cudaEventDisableTiming | cudaEventBlockingSync);
+        return cudaEventCreateWithFlags(&event_, cudaEventDisableTiming);
     }
 
     cudaError_t Record(void *stream)
